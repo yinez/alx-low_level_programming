6b57a6c62a89cd828eb2157707d315c7979417a5
yinez <yinebebzergaw@gmail.com>
2021-09-23 11:04:39 -0700

    my project

---
+++ 0x0C-more_malloc_free/1-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+	char *concat;
+
+	concat = string_nconcat("Holberton ", "School !!!", 6);
+	printf("%s\n", concat);
+	free(concat);
+	return (0);
+}
